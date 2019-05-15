d4ead5446acf1ede40d13e1dc78d1ef680c99507
Thomas <thomas.hofmann2003@gmail.com>
2019-05-15 19:45:05 +0200

    Adopted to newest Version of AWTS

--- src/thomas/hofmann/MainApp.java
+++ src/thomas/hofmann/MainApp.java
@@ -4,111 +4,95 @@ import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
+public class MainApp implements ContextInterface {
+	int x, y;
 
-public class MainApp implements Engine
-{
-	int x,y;
-	int width,height;
-	public static void main(String[] args)
-	{
-		//Creating a new Object of this Class
-		//since the whole Engine can't be accessed via a static function
+	public static void main(String[] args) {
+		// Creating a new Object of this Class
+		// since the ContextPane can't be accessed via a static function
 		new MainApp().init();
 	}
-	
-	void init()
-	{
-		//Creating the Window in which the GraphicsEngine should
-		//display the drawn stuff
+
+	void init() {
+		// Creating the Window in which the ContextPane should
+		// display the drawn stuff
 		Window main = new Window("CodeExample", 1000, 600, true);
-		//Creating the a Graphics Engine giving it the Window it should display on
-		//and the class that contains the draw, loop, keyPress, etc. functions
-		GraphicsEngine ge = new GraphicsEngine(main, this);
-		//Starts a Thread which runs the loop function
+		ContextPane ge = new ContextPane(main, this);
+		// Start the execution of the loop function
 		ge.start();
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e)
-	{
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e)
-	{
-		
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e)
-	{
-		
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent e)
-	{
-		
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e)
-	{
-		//If the mouse moved:update location of the Rectangle
-		x=e.getX();
-		//Since the coordinate system is flipped, the y axis has to be flipped to
-		y=-e.getY()+this.height;
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e)
-	{
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e)
-	{
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e)
-	{
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e)
-	{
-		//Writes 'pressed' to the console every time
-		//a mouse button was pressed
-		System.out.println("pressed");
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e)
-	{
-		
-	}
-
-	@Override
-	public void draw(Graphics2D g)
-	{
-		//Draws a Rectangle at the location of the mouse
+	public void draw(Graphics2D g) {
+		// Coordinate system starts at top left
+		// Draws a Rectangle at the location of the mouse
 		g.fillRect(x, y, 100, 100);
 	}
 
 	@Override
-	public void loop(GraphicsEngine ge)
-	{
-		//Updates the width and height to calculate the mouse-path
-		this.width=ge.width;
-		this.height=ge.height;
-		//Redraws the whole screen
+	public void drawRev(Graphics2D g) {
+		// Coordinate system starts at bottom left
+	}
+
+	@Override
+	public void loop(ContextPane ge) {
+		// Redraws the whole screen
 		ge.redraw();
-		//Caps the frame rate at 60 fps 
+		// Caps the frame rate at 60 frames per second
 		ge.capFrameRate(60);
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// If the mouse moved, update x and y position
+		x = e.getX();
+		y = e.getY();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// Writes 'pressed' to the console every time
+		// a mouse button was pressed
+		System.out.println("pressed");
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+
 	}
 }
